e4aacb6681739da0e56369d0db86175e78dc4337
DanielNoblero <110746994+DanielNoblero@users.noreply.github.com>
2025-08-02 10:41:38 -0300

    Switch to HashRouter and update Vite base config
    Replaced BrowserRouter with HashRouter in main.jsx to improve routing compatibility, and updated the Vite config to use a trailing comma in the base path. This change ensures the app works correctly with client-side routing in different deployment environments.

--- Inmobiliaria/src/main.jsx
+++ Inmobiliaria/src/main.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom'; 
 import App from './App.jsx';
-import { AuthProvider } from './context/AuthContext'; // ✅ Importación necesaria
+import { AuthProvider } from './context/AuthContext';
 import './components/Css/style.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter basename="/inmobiliaria">
+  <HashRouter > 
     <AuthProvider>
       <App />
     </AuthProvider>
-  </BrowserRouter>
+  </HashRouter>
 );
